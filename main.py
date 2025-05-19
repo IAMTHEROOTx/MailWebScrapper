873818ae30f82db6788e04dc7dea0cc25209e57a
IAMTHEROOT <cybsecroot@gmail.com>
2025-05-19 14:33:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def extract_emails_from_url(url):
         response = requests.get(url)
         response.raise_for_status()
         html_content = response.text
-        # Simple regex for emails
         email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
         emails = re.findall(email_pattern, html_content)
         unique_emails = set(emails)
